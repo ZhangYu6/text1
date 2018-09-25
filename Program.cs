d208f8a83a9064e134e599f0810064cc9b2b93cf
ZhangYu6 <zhangyu1203>
2018-09-25 23:21:39 +0800

    写了第一行

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace text1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("this is my first line");
         }
     }
 }
