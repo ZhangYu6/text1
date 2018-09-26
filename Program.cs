44f199256f2c138689e027cd1502b34d429e0baa
ZhangYu6 <zhangyu1203>
2018-09-26 22:55:30 +0800

    加入了一句

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ namespace text1
     {
         static void Main(string[] args)
         {
+            int a=5;
+            if(a<10)
+            {
             Console.WriteLine("this is my first line");
+            }
+            else
+            {
             Console.WriteLine("this is my second line");
+            Console.Beep(1000,1000);
+            }
         }
     }
 }
